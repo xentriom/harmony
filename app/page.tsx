f184ebc7b82e0084444aed8504da836e1a13e8b6
xentriom <c04jason@gmail.com>
2025-03-03 16:35:28 -0500

    set background color for page

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function LandingPage() {
     <div className="relative h-screen w-full">
       {/* Nav Bar */}
       <div className="fixed top-0 left-0 right-0 p-4 z-50">
-        <nav className="w-full h-18 bg-gray-800 shadow-lg p-4 rounded-lg flex justify-between items-center">
+        <nav className="w-full bg-slate-800 shadow-lg p-4 rounded-lg flex justify-between items-center">
           <div className="flex flex-row items-center gap-2">
             <Image src="/logo.svg" alt="Harmony Logo" width={40} height={40} />
             <h1 className="text-2xl font-semibold">Harmony</h1>
