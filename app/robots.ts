793ca9e84bffb6cc6efc7636c432afb015734229
xentriom <114116827+xentriom@users.noreply.github.com>
2025-03-03 12:24:32 -0500

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/"
     },
-    sitemap: "https://harm-ony.vercel.app/sitemap.xml"
+    sitemap: "https://tndc.vercel.app/sitemap.xml"
   };
 }
