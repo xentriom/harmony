e9aede5c75774351272de53665477fc25359f62e
xentriom <c04jason@gmail.com>
2025-03-04 17:48:27 -0500

    ignore landing for pwa

--- app/manifest.ts
+++ app/manifest.ts
@@ -5,7 +5,7 @@ export default function manifest(): MetadataRoute.Manifest {
     name: "Harmony - Group Chat That's All Fun & Games",
     short_name: "Harmony",
     description: "Harmony is great for playing games and chilling with friends, or even building a worldwide community. Customize your own space to talk, play, and hang out.",
-    start_url: "/",
+    start_url: "/home",
     display: "standalone",
     background_color: "#0f172b",
     theme_color: "#0f172b",
