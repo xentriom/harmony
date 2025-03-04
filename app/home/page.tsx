c381d3fad2a04b2459278bf555ec7a24a8994aba
xentriom <c04jason@gmail.com>
2025-03-04 10:14:59 -0500

    adjusted middleware

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,3 +1,10 @@
-export default function App() {
-  return <div>Redirecting to Harmony...</div>;
+import { redirect } from "next/navigation";
+import { createClient } from "@/lib/supabase/server";
+
+export default async function App() {
+  const supabase = await createClient();
+  const { data, error } = await supabase.auth.getUser();
+  if (error || !data?.user) {
+    redirect("/login");
+  }
 }
